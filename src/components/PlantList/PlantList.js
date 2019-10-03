dc35da4b7534520e5f438dc2685097562c406fc0
Nathan Wold <natew79@gmail.com>
2019-10-03 14:30:39 -0500

    get call working

--- src/components/PlantList/PlantList.js
+++ src/components/PlantList/PlantList.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-const mapStateToProps = reduxState => ({
-    reduxState,
-});
 
 class PlantList extends Component {
     componentDidMount() {
         // use component did mount to dispatch an action to request the plantList from the API
+        this.getPlant();
+    }
+
+    getPlant = () => {
+        this.props.dispatch({ type: 'GET_PLANT' })
     }
 
     render() {
         return (
             <div>
                 <h3>This is the plant list</h3>
-                <pre>{JSON.stringify(this.props.reduxState)}</pre>
+                <pre>{JSON.stringify(this.props.reduxState.plantList)}</pre>
             </div>
         );
     }
 }
+
+const mapStateToProps = reduxState => ({
+    reduxState,
+});
 
 export default connect(mapStateToProps)(PlantList);
--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+// import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { takeEvery, put } from 'redux-saga/effects';
+import logger from 'redux-logger';
+import axios from 'axios';
 
 import App from './App';
+import createSagaMiddleware from 'redux-saga';
 
-// this startingPlantArray should eventually be removed
-const startingPlantArray = [
-  { id: 1, name: 'Rose' },
-  { id: 2, name: 'Tulip' },
-  { id: 3, name: 'Oak' }
-];
+const sagaMiddleware = createSagaMiddleware();
 
-const plantList = (state = startingPlantArray, action) => {
+function* rootSaga() {
+  yield takeEvery('GET_PLANT', getPlant)
+}
+
+function* getPlant() {
+  try {
+    const response = yield axios.get('/api/plant');
+    console.log(response.data);
+    yield put({ type: 'SET_PLANT', payload: response.data });
+  } catch (error) {
+    console.log('error in GET', error);
+  }
+}
+
+const plantList = (state = [], action) => {
+  console.log(action);
+  
   switch (action.type) {
-    case 'ADD_PLANT':
-      return [ ...state, action.payload ]
+    case 'SET_PLANT':
+      state = action.payload;
+      return state
     default:
       return state;
   }
@@ -23,6 +40,10 @@ const plantList = (state = startingPlantArray, action) => {
 
 const store = createStore(
   combineReducers({ plantList }),
+  applyMiddleware(sagaMiddleware, logger)
 );
 
+sagaMiddleware.run(rootSaga);
+
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
+// registerServiceWorker();
